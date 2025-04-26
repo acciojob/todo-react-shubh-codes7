1e5f60f7d81c5883260aa81342a96c4dd5dfa172
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-26 18:07:42 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Todo from './Todo.js'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        app
+        <Todo />
     </div>
   )
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Todo(){
+    const [todo, setTodo] = React.useState('')
+    const [todoarr, setTodoarr] = React.useState([])
+
+    function handleAdd(e){
+        setTodoarr((prev) => [todo, ...prev])
+        setTodo('')
+    }
+    
+    function handleDelete(idx){
+        let filteredArr = todoarr.filter((todo, id) => id !== idx)
+        setTodoarr(filteredArr)
+    }
+
+    return(
+        <>
+            <h2>To-Do List</h2>
+            <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+            <button onClick={handleAdd}>Add Todo</button>
+            <div>
+                {
+                    todoarr.length > 0 && todoarr.map((todo, idx) => {
+                        return(
+                        <div key={idx}>
+                            <p>{todo}</p>
+                            <button onClick={()=>handleDelete(idx)}>Delete</button>
+                        </div>)
+                    })
+                }
+            </div>
+        </>
+    )
+}
